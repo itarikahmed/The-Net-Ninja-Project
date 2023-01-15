b52ff8220fd43b21de8ccaa05942c7b50719d87a
Tarik-Ahmed75 <tarik.ahmed@northsouth.edu>
2023-01-15 22:48:35 +0600

    project created

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
