2d1539968a64580cff74f8ed9451b07ae3793b97
Tarik-Ahmed75 <tarik.ahmed@northsouth.edu>
2023-01-19 04:13:18 +0600

    route parameters done

--- src/component/BlogList.jsx
+++ src/component/BlogList.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 // const BlogList = ({ blogs, title, handleDelete })
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div>
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p> Written by{blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
           {/* <button onClick={() => handleDelete(blog.id)}>Delete Blog</button> */}
         </div>
       ))}
