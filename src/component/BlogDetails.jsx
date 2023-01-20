13f0a43f76787939f5ab5de97880ec34c5c62e71
Tarik-Ahmed75 <tarik.ahmed@northsouth.edu>
2023-01-20 23:12:29 +0600

    project done

--- src/component/BlogDetails.jsx
+++ src/component/BlogDetails.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useNavigate, useParams } from "react-router-dom";
+
 const BlogDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => navigate("/"));
+  };
   return (
-    <div>
-      <h2>Blog details-{id}</h2>
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
+        </article>
+      )}
     </div>
   );
 };
--- src/component/Create.jsx
+++ src/component/Create.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Niloy");
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const blog = { title, body, author };
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => navigate("/"));
+  }
+
   return (
     <div className="create">
       <h1>Create new Blogs</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <label>Body</label>
+        <textarea
+          type="text"
+          required
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+        />
+        <select
+          value={author}
+          onChange={(event) => {
+            setAuthor(event.target.value);
+          }}
+        >
+          <option value="Niloy">Niloy</option>
+          <option value="Nasim">Nasim</option>
+          <option value="Saif">Saif</option>
+        </select>
+        <button>Submit Blog</button>
+      </form>
     </div>
   );
 };
